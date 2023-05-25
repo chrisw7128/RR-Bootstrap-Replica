2e7eb0a2ec970e237657be4857261f6a075668f9
Chris Wehner <chrisw7128@gmail.com>
2023-05-24 21:30:41 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import "./App.css";
+import splashImage from "./embedded/background.webp";
+import TopBar from "./components/TopBar";
+import styled from "styled-components";
+import StoreListing from "./components/StoreListing";
+
+const Splash = styled.div`
+  width: 100vw;
+  height: 250px;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-image: url(${splashImage});
+  background-color: #deedd6;
+  background-position: right;
+  z-index: -1;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <TopBar />
+      <Splash />
+      <StoreListing />
+    </>
   );
 }
 
